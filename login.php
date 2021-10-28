329e81281a29d05eadcb7fcff0bf7caabe7eb0ed
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-10-27 23:47:39 -0300

    Updadte

--- login.php
+++ login.php
@@ -4,8 +4,14 @@ Nome: Leonardo Bernardes de Oliveira
 RA: 834280  
 */
 
+// Declarando variaveis de login de exemplo.
+$usuario = 'teste';
+$senha = '1234';
 
-
+if (empty($_POST['usuario'] || empty($_POST['senha']))) {
+    header('Location: index.php');
+    exit();
+}
 
 
 
